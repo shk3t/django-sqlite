19b60a0af45336922cc3f956a9140357add9244a
shk3t <daniildudov@gmail.com>
2024-05-04 15:30:32 +0300

    injectfiles -> files; stubs

---
+++ stubs/main/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Todo(models.Model):
+    pass  # Implement your model here
+
+
+class Collection(models.Model):
+    pass  # Implement your model here
---
+++ stubs/main/views.py
@@ -0,0 +1,6 @@
+def todo_view(request, id):
+    pass  # Implement your view here
+
+
+def todos_view(request):
+    pass  # Implement your view here
